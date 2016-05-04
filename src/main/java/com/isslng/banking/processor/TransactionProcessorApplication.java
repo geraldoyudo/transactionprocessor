be7aecdfd29001f90dcbeb9d85bf41798f38f972
Gerald Oyudo <gerald.oyudo@isslng.com>
2016-05-04 10:52:40 +0100

    saved production configuration

--- src/main/java/com/isslng/banking/processor/TransactionProcessorApplication.java
+++ src/main/java/com/isslng/banking/processor/TransactionProcessorApplication.java
@@ -2,11 +2,18 @@ package com.isslng.banking.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TransactionProcessorApplication {
+public class TransactionProcessorApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionProcessorApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TransactionProcessorApplication.class);
+    }
+
 }
